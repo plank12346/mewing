fa37af085cfc28fa8dfe015c328182fe6961bf9a
plank12346 <wachirawat.jr@gmail.com>
2025-02-01 21:58:38 +0700

    skin + user not found

--- server.js
+++ server.js
@@ -11,31 +11,59 @@ app.listen(3000, () => {
     console.log('Start server at port 3000.')
   })
 
-  app.get('/user', (req, res) => {
+app.get('/user', (req, res) => {
+  console.log("All user ")
+
     res.json(UserTable)
-  })  
+})
+  
 
 app.post('/user/register', (req, res) => {
-  const userindex = UserTable.find(user => user.id === req.body.id)
+  console.log("Register ")
+
+  const user = UserTable.find(user => user.id === req.body.id)
   console.log(UserTable.length)
-  console.log(userindex)
-  if (typeof(userindex) == "undefined"){
+  console.log(user)
+  if (user === undefined){
     UserTable.push(req.body)
-    res.status(201).json(req.body)
+    res.status(200).json(req.body)
   }else {
-    res.send('User is existing')
+    res.status(400).send('User is existing')
   }
 })
 
-app.get('/user/:id', (req, res) => {
-  console.log("Profile")
-  res.json(UserTable.find(user => user.id === req.params.id))
+app.get('/user/profile/:id', (req, res) => {
+  console.log("Profile " + req.params.id)
+
+  const user = UserTable.find(user => user.id === req.params.id)
+  console.log(UserTable.length)
+  console.log(user)
+  if (user === undefined){
+    res.status(404).send('User not found')
+  }else {
+    console.log(user)
+
+    res.status(200).json(user)
+  }  
 })
 
 app.put('/user', (req, res) => {
   console.log("Skin")
-  const updateIndex = user.findIndex(user => user.id === req.body.id)
-  res.json(Object.assign(UserTable[updateIndex].skin, req.body.skin))
+  console.log(req.body)
+
+  const updateIndex = UserTable.findIndex(user => user.id === req.body.id)
+  console.log(UserTable.length)
+  console.log("index :" + updateIndex)
+
+  if (updateIndex < 0){
+    res.status(404).send('User not found')
+  }else {
+    user = UserTable[updateIndex]
+    user.skin = req.body.skin
+    console.log(user)
+
+    res.status(200).json(Object.assign(UserTable[updateIndex], user))
+  }  
 })
 
 app.post('/user/login', (req, res) => {
