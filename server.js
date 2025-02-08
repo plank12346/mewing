bc31b4dac9d342eabb02c22f23ed30b1f1eaf9ff
Tummeng <pathomchoke.j@gmail.com>
2025-02-09 01:19:22 +0700

    add ranking

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express()
 const UserTableByEmail = new Map();
 const UserTableByID = new Map();
 
+const StepTable = new Map();
+const RankingRawTable = new Map();
+const RankingTable = new Map();
+
 const bodyParser = require('body-parser')
 
 function formatDateTime(dateInput) {
@@ -31,8 +35,33 @@ function formatDate(dateInput) {
   return `${year}-${month}-${day}`;
 }
 
+function getUserByID(id) {
+  const user = UserTableByID.get(id)
+  console.log(user)
+
+  const userInfo = {
+    "id": user.id,
+    "name": user.name,
+    "email": user.email,
+    "dob": user.dob,
+    "skin": user.skin,
+    "class": user.class
+  }
+
+  return userInfo
+}
+
 function getUserTable() {
   return Object.fromEntries(UserTableByEmail)
+}
+
+function mapToObject(map) {
+  if (map instanceof Map) {
+    return Object.fromEntries(
+      Array.from(map.entries()).map(([key, value]) => [key, mapToObject(value)])
+    );
+  }
+  return map; // Return the value directly if it's not a Map
 }
 
 app.use(bodyParser.json())
@@ -42,16 +71,23 @@ app.listen(3000, () => {
     console.log('Start server at port 3000.')
   })
 
+  
+app.get('/ver', (req, res) => {
+  console.log("----------- version -----------")
+
+  return res.send("1.0.0")
+})
+
 app.get('/', (req, res) => {
   console.log("----------- host -----------")
 
-    res.json(getUserTable())
+  return res.json(getUserTable())
 })
 
 app.get('/user', (req, res) => {
   console.log("----------- All user -----------")
 
-    res.json(getUserTable())
+  return res.json(getUserTable())
 })
 
 app.post('/user-table', (req, res) => {
@@ -100,9 +136,9 @@ app.post('/user/register', (req, res) => {
       "skin": newUser.skin,
       "class": newUser.class
     }
-    res.status(200).json(response)
+    return res.status(200).json(response)
   } else {
-    res.status(400).send('User is existing')
+    return res.status(400).send('User is existing')
   }
 })
 
@@ -110,20 +146,9 @@ app.get('/user/profile/:id', (req, res) => {
   console.log("----------- Profile " + req.params.id + " -----------")
 
   if (!UserTableByID.has(req.params.id)) {
-    res.status(404).send('User not found')
+    return res.status(404).send('User not found')
   } else {
-    const user = UserTableByID.get(req.params.id)
-    console.log(user)
-
-    response = {
-      "id": user.id,
-      "name": user.name,
-      "email": user.email,
-      "dob": user.dob,
-      "skin": user.skin,
-      "class": user.class
-    }
-    res.status(200).json(response)
+    return res.status(200).json(getUserByID(req.params.id))   
   }  
 })
 
@@ -132,7 +157,7 @@ app.put('/user', (req, res) => {
   console.log(req.body)
 
   if (!UserTableByID.has(req.body.id)) {
-    res.status(404).send('User not found')
+    return res.status(404).send('User not found')
   } else {
     const user = UserTableByID.get(req.body.id)
     user.skin = req.body.skin
@@ -149,7 +174,7 @@ app.put('/user', (req, res) => {
       "skin": user.skin,
       "class": user.class
     }
-    res.status(200).json(response)
+    return res.status(200).json(response)
   }  
 })
 
@@ -157,7 +182,7 @@ app.post('/user/login', (req, res) => {
   console.log("----------- Login -----------")
 
   if (!UserTableByEmail.has(req.body.email)) {
-    res.status(404).send('User not found')
+    return res.status(404).send('User not found')
   } else {
     const user = UserTableByEmail.get(req.body.email)
 
@@ -171,43 +196,96 @@ app.post('/user/login', (req, res) => {
         "skin": user.skin,
         "class": user.class
       }
-      res.status(200).json(response)
+      return res.status(200).json(response)
     } else {
-      res.status(400).send('password is incorrect')
+      return res.status(400).send('password is incorrect')
     }
   }
 })
 
 app.post('/user/forget', (req, res) => {
   console.log("----------- Forgetpassword -----------")
-  res.send('ForgetPassword')
+  return res.send('ForgetPassword')
 })
 
 
 app.post('/user/step', (req, res) => {
   console.log("----------- Add Step -----------")
 
-
   if (!UserTableByID.has(req.body.id)) {
-    res.status(404).send('User not found')
-  } else {
-    const user = UserTableByID.get(req.params.id)
-    // TODO: save data to DB
-    // structure
-    // {
-    //   "id":"id",
-    //   "date": "2025-02-05",
-    //   "steps": 20
-    // }
-    res.status(200).send('step')
-  }  
+    return res.status(404).send('User not found')
+  } 
+
+  const userInfo = getUserByID(req.body.id)
+
+  const stepInfoByDate = RankingRawTable.get(req.body.date) || new Map();
+  const stepInfoByClass = stepInfoByDate.get(userInfo.class) || new Map();
+  stepInfoByClass.set(req.body.id, req.body.step)
+  stepInfoByDate.set(userInfo.class, stepInfoByClass);
+  RankingRawTable.set(req.body.date, stepInfoByDate)
+  // console.log(stepInfoByClass)
+  // console.log(stepInfoByDate)
+  // console.log(RankingRawTable)
+
+
+  const userStepData = StepTable.get(req.body.id) || new Map(); 
+  userStepData.set(req.body.date, req.body.step)
+  StepTable.set(req.body.id, userStepData)
+  // console.log(userStepData)
+  // console.log(StepTable)
+
+  res.status(200).json(Object.fromEntries(userStepData))
+})
+
+app.get('/ranking-raw-table', (req, res) => {
+  console.log("----------- Get Ranking Table -----------")
+
+  res.status(200).json(mapToObject(RankingRawTable))
+})
+
+app.get('/ranking-table', (req, res) => {
+  console.log("----------- Get Ranking Table -----------")
+
+  res.status(200).json(mapToObject(RankingTable))
+})
+
+app.get('/step-table', (req, res) => {
+  console.log("----------- Get Step Table -----------")
+
+  res.status(200).json(mapToObject(StepTable))
 })
 
 app.get('/ranking/:date', (req, res) => {
   console.log("----------- Get Ranking [" + req.params.date + "] -----------")
 
-  // TODO: calculate ranking of each day
-  res.status(200).send('Get Ranking [' + req.params.date + ']')
+  if (!RankingTable.has(req.params.date)) {
+    return res.status(404).send('No Data')
+  } 
+
+  return res.status(200).send(RankingTable.get(req.params.date))
+})
+
+app.get('/ranking-calculate/:date', (req, res) => {
+  console.log("----------- Calculate Ranking [" + req.params.date + "] -----------")
+
+  if (!RankingRawTable.has(req.params.date)) {
+    return res.status(404).send('No Data')
+  } 
+
+  const result = Array.from(RankingRawTable.get(req.params.date))
+    .map(([className, steps]) => {
+      const totalSteps = Array.from(steps.values()).reduce((acc, val) => acc + val, 0);
+      return {
+        className,
+        sum: totalSteps
+      };
+    })
+    .sort((a, b) => b.sum - a.sum);
+  console.log(result)
+
+  RankingTable.set(req.params.date, result)
+
+  return res.status(200).send(result)
 })
 
 app.post('/ranking', (req, res) => {
